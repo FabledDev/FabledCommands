716c3b2eeac662f8e8526396c64c1f0a397ba29f
Mantice <nicholas_call@outlook.com>
2024-04-14 09:09:09 -0400

    feat: constructor logic

--- src/main/java/dev/fabled/fabledcommands/FabledSubCommand.java
+++ src/main/java/dev/fabled/fabledcommands/FabledSubCommand.java
@@ -1,15 +1,20 @@
 package dev.fabled.fabledcommands;
 
 import com.mojang.brigadier.context.CommandContext;
+import dev.fabled.fabledcommands.annotations.LiteralArg;
+import dev.fabled.fabledcommands.annotations.RequiredArg;
 import dev.fabled.fabledcommands.exceptions.InvalidSubCommandException;
 import net.minecraft.commands.CommandSourceStack;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
+import java.lang.reflect.Method;
+import java.util.*;
+
 public abstract class FabledSubCommand {
 
-    private final ArgBuilder<CommandSourceStack> argBuilder;
+    protected final ArgBuilder<CommandSourceStack> argBuilder;
 
     public FabledSubCommand() throws InvalidSubCommandException {
         argBuilder = ArgBuilder.fromClass(this.getClass());
@@ -21,6 +26,46 @@ public abstract class FabledSubCommand {
 
             return player(player, context);
         });
+
+        final Map<Integer, ArgBuilder<CommandSourceStack>> methodMap = new HashMap<>();
+        final Method[] methods = this.getClass().getMethods();
+
+        for (final Method method : methods) {
+            final ArgBuilder<CommandSourceStack> methodBuilder = ArgBuilder.fromMethod(method);
+            int pos;
+
+            if (method.isAnnotationPresent(LiteralArg.class)) {
+                final LiteralArg arg = method.getAnnotation(LiteralArg.class);
+                pos = arg.position();
+            }
+
+            else {
+                final RequiredArg arg = method.getAnnotation(RequiredArg.class);
+                pos = arg.position();
+            }
+
+            methodMap.put(pos, methodBuilder);
+        }
+
+        final List<Integer> keys = new ArrayList<>(methodMap.keySet());
+        Collections.sort(keys);
+
+        ArgBuilder<CommandSourceStack> argBuilders = null;
+        for (int key : keys) {
+            final ArgBuilder<CommandSourceStack> arg = methodMap.get(key);
+            if (argBuilders == null) {
+                argBuilders = arg;
+                continue;
+            }
+
+            argBuilders.then(arg);
+        }
+
+        if (argBuilders == null) {
+            return;
+        }
+
+        argBuilder.then(argBuilders);
     }
 
     public abstract int console(@NotNull final CommandSender sender, @NotNull final CommandContext<CommandSourceStack> context);
