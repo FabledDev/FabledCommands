fbdd60a06a6c2837afdcba94c965d429c321ca9b
Mantice <nicholas_call@outlook.com>
2024-04-14 09:06:37 -0400

    feat: made static methods public

--- src/main/java/dev/fabled/fabledcommands/BrigadierUtils.java
+++ src/main/java/dev/fabled/fabledcommands/BrigadierUtils.java
@@ -10,15 +10,15 @@ import org.jetbrains.annotations.NotNull;
 
 public final class BrigadierUtils {
 
-    static LiteralArgumentBuilder<CommandSourceStack> literal(@NotNull final String name) {
+    public static LiteralArgumentBuilder<CommandSourceStack> literal(@NotNull final String name) {
         return LiteralArgumentBuilder.literal(name);
     }
 
-    static <T> RequiredArgumentBuilder<CommandSourceStack, T> required(@NotNull final String name, @NotNull final ArgumentType<T> type) {
+    public static <T> RequiredArgumentBuilder<CommandSourceStack, T> required(@NotNull final String name, @NotNull final ArgumentType<T> type) {
         return RequiredArgumentBuilder.argument(name, type);
     }
 
-    static CommandSender getSender(@NotNull final CommandContext<CommandSourceStack> context) {
+    public static CommandSender getSender(@NotNull final CommandContext<CommandSourceStack> context) {
         return context.getSource().getBukkitSender();
     }
 
