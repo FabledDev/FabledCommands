3f3ebcca7e869a1c494f3d954872fc4d0594eb2c
Mantice <nicholas_call@outlook.com>
2024-04-14 09:07:43 -0400

    feat: command information fields, fallback prefix, constructor logic

--- src/main/java/dev/fabled/fabledcommands/FabledCommand.java
+++ src/main/java/dev/fabled/fabledcommands/FabledCommand.java
@@ -1,17 +1,39 @@
 package dev.fabled.fabledcommands;
 
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.tree.CommandNode;
+import dev.fabled.fabledcommands.annotations.Command;
+import dev.fabled.fabledcommands.exceptions.InvalidCommandException;
 import net.minecraft.commands.CommandSourceStack;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public abstract class FabledCommand {
 
-    final LiteralArgumentBuilder<CommandSourceStack> literalArgumentBuilder;
+    final String name;
+    final String[] aliases;
+    final String permission;
+    final String description;
+    final String usage;
+    final CommandNode<CommandSourceStack> node;
+    private String fallbackPrefix;
 
-    public FabledCommand(@NotNull final FabledSubCommand... subCommands) {
-        literalArgumentBuilder = BrigadierUtils.literal("")
+    public FabledCommand(@NotNull final FabledSubCommand... subCommands) throws InvalidCommandException {
+        final Class<?> clazz = this.getClass();
+        if (!clazz.isAnnotationPresent(Command.class)) {
+            throw new InvalidCommandException(clazz);
+        }
+
+        final Command commandInfo = clazz.getAnnotation(Command.class);
+        name = commandInfo.name().toLowerCase();
+        aliases = commandInfo.aliases();
+        permission = commandInfo.permission();
+        description = commandInfo.description();
+        usage = commandInfo.usage();
+
+        final LiteralArgumentBuilder<CommandSourceStack> builder = BrigadierUtils.literal(name)
                 .executes(context -> {
                     final CommandSender sender = BrigadierUtils.getSender(context);
                     if (!(sender instanceof Player player)) {
@@ -20,6 +42,20 @@ public abstract class FabledCommand {
 
                     return player(player);
                 });
+
+        for (final FabledSubCommand subCommand : subCommands) {
+            builder.then(subCommand.argBuilder.build());
+        }
+
+        node = builder.build();
+    }
+
+    public void setFallbackPrefix(@NotNull final String fallbackPrefix) {
+        this.fallbackPrefix = fallbackPrefix;
+    }
+
+    public @Nullable String getFallbackPrefix() {
+        return fallbackPrefix;
     }
 
     public abstract int console(@NotNull final CommandSender sender);
